8660923557f181399942d92086cfed0fa7a5275f
garzadekoster <garzadekoster@users.noreply.github.com>
2018-07-09 14:11:20 -0500

    Update nginx-autoinstall.sh

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -450,21 +450,21 @@ case $OPTION in
 		--with-http_auth_request_module \
 		--with-http_slice_module \
 		--with-http_stub_status_module \
-		--with-http_realip_module" \
-		--with-pcre-jit
-                --with-ipv6
-                --with-http_dav_module 
-                --with-http_flv_module 
-                --with-http_gzip_static_module 
-                --with-http_image_filter_module 
-                --with-http_mp4_module 
-                --with-http_random_index_module 
-                --with-http_secure_link_module 
-                --with-http_spdy_module 
-                --with-http_sub_module 
-                --with-http_xslt_module 
-                --with-mail 
-                --with-mail_ssl_module
+		--with-http_realip_module \
+		--with-pcre-jit \
+                --with-ipv6 \
+                --with-http_dav_module \ 
+                --with-http_flv_module \
+                --with-http_gzip_static_module \
+                --with-http_image_filter_module \
+                --with-http_mp4_module \ 
+                --with-http_random_index_module \ 
+                --with-http_secure_link_module \ 
+                --with-http_spdy_module \
+                --with-http_sub_module \
+                --with-http_xslt_module \
+                --with-mail \
+                --with-mail_ssl_module"
 
 		# Optional modules
 		# LibreSSL 
